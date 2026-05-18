e7caa0c8870f438e3e2279393d40e5fc7c03d6d4
Prsh8045 <prashantprs12@gmail.com>
2026-05-18 10:17:38 +0530

    add remove element

--- index.js
+++ index.js
@@ -9,3 +9,16 @@ var removeDuplicates = function(nums) {
     }
     return x+1;
 };
+
+//Remove Element
+var removeElement = function(nums, val) {
+    let x=0;
+    for(i=0;i<nums.length;i++){
+        if(nums[i]!=val){
+            nums[x]=nums[i];
+            x=x+1;
+        }
+    }
+    return x;
+    
+};
