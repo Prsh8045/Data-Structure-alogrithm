c0c3f0f59bf6ee31ac78a4a365d6c6eb292bf3ad
Prsh8045 <prashantprs12@gmail.com>
2026-05-19 23:36:11 +0530

    add power of two

--- index.js
+++ index.js
@@ -22,3 +22,44 @@ var removeElement = function(nums, val) {
     return x;
     
 };
+
+//sum of all element in array
+arr=[2,3,4,5,6];
+function sum(n){
+    if(n==0){
+        return arr[n]
+    }
+    return arr[n]+sum(n-1)
+}
+let result=sum(arr.length-1);
+console.log(result);
+
+//sum of all odd numbers in the array.
+let arr=[5,8,2,10,1,3] 
+
+function sum(n){
+          let isOdd=arr[n]%2!=0; 
+          if(n==0){return isOdd?arr[n]:0}
+
+        
+     return (isOdd?arr[n]:0)+sum(n-1)                                               
+}
+console.log(sum(arr.length-1))
+// Find the factorial of a number using recursion
+let n=5
+function fact(n){
+    if(n==1){
+        return 1;
+    }
+    return n*fact(n-1)
+}
+console.log(fact(n))
+//Power of two
+let n=5
+var isPowerOfTwo = function(n) {
+    if(n==1) return true;
+    else if ((n%2 !==0)|| n<1) return false;
+    return isPowerOfTwo(n/2)
+    
+};
+console.log(isPowerOfTwo(n))
