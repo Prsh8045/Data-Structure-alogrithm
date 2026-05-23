9e276609ea3f89f4339031766e75b69c537f2a3a
Prsh8045 <prashantprs12@gmail.com>
2026-05-23 22:56:05 +0530

    add linear search

--- index.js
+++ index.js
@@ -100,3 +100,16 @@ let temp=a;
 a=b;
 b=temp;
 console.log(a,b)
+
+//Linear search
+let arr=[4,9,1,0,2,10];
+function linearSearch(arr,target){
+    for(let i=0;i<arr.length;i++){
+        if(arr[i]===target){
+            return i;
+        }
+    }
+    return -1;  
+}
+let result=linearSearch(arr,10);
+console.log(result)
